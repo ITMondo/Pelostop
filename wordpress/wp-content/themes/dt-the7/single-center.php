450cbe76a05d61addfda7aa70d2432cb0c1bd8b6
Lisa <lisahornung@gmx.de>
2018-08-12 19:18:09 +0200

    added list under maps

--- wordpress/wp-content/themes/dt-the7/single-center.php
+++ wordpress/wp-content/themes/dt-the7/single-center.php
@@ -18,19 +18,17 @@ get_header( 'single' );
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
 	class Center {
-    private $fields = ['longitude', 'latitude', 'google_place_id'];
+    private $fields = ['longitude', 'latitude', 'google_place_id', 'street', 'number', 'door', 'zipcode', 'town', 'province', 'telephone','email', 'opening_hours'];
 
 		function __construct($post) {
 			$post_meta = get_post_meta($post->ID);
 			$this->title = $post->post_title;
 
-
 			foreach($this->fields as $field){
 				$this->$field = $post_meta[$field][0];
 			}
-
-			//$attributes_json = json_encode($attributes);
 		}
+
 
 
 		public $title;
@@ -44,53 +42,63 @@ get_header( 'single' );
 		}
 	}
 
-	//get_attributes($$field);
-
   $center = new Center($post);
 ?>
-	adhs (above content)
 
 	<?php get_template_part( 'header-main' ) ?>
-joo (down)
 
 	<?php if ( presscore_is_content_visible() ): ?>
-hurn (down)
-		<?php do_action( 'presscore_before_loop' ) ?>
-hello (down)
-		<div id="content" class="content" role="main">
-content
 
-				<iframe
+		<?php do_action( 'presscore_before_loop' ) ?>
+
+		<div id="content" class="content" role="main">
+
+			<iframe
   				width="600"
   				height="450"
   				frameborder="0" style="border:0"
   				src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCYd8IA-EXnP5i9eUGf3WeIJpj3nOFuVVA&q=place_id:<?php echo $center->google_place_id; ?>" allowfullscreen>
-</iframe>
+			</iframe>
+
+			<div id="center_meta">
+				<ul>
+				<li>
+	        Dirección: <?php echo $center->street ;?>,
+					<?php echo $center->number; ?>.
+					<?php echo $center->zipcode; ?>
+					<?php echo $center->town; ?>
+					(<?php echo $center->province; ?>)
+				</li>
+				<li>
+	        Horario: <?php echo $center->opening_hours;?>
+				</li>
+				<li>
+	        Teléfono: <?php echo $center->telephone;?>
+				</li>
+				<li>
+	        E-mail: <?php echo $center->email;?>
+				</li>
+			</ul>
 			</div>
 
 
 
-
 			<?php if ( post_password_required() ): ?>
-				steht nirgends
-				<article id="post-<?php the_ID() ?>" <?php post_class() ?>>
-steht nirgends
+
+				<article id="post-<?php the_ID() ?>" <?php post_class() ?>
+
 					<?php
 					do_action( 'presscore_before_post_content' );
 
 					the_content();
 
-					do_action( 'presscore_after_post_content' );
-
-					?>
+					do_action( 'presscore_after_post_content' );?>
 
 				</article>
 
 			<?php else: ?>
 
 				<?php get_template_part( 'content-single', str_replace( 'dt_', '', get_post_type() ) ) ?>
-
-				echo "content";
 
 			<?php endif ?>
 
